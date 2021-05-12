65bed1239dbd791fa9431ae24c84833e7d95be98
Atarios17 <83716600+Atarios17@users.noreply.github.com>
2021-05-12 17:47:32 +0200

    Update Projekt_WDIF_1.R

--- Projekt_WDIF_1.R
+++ Projekt_WDIF_1.R
@@ -1,42 +1,3 @@
-#Ktoś może od nas kupić
-EOP_call <- function(S0,r,K,Time,dt,sigma){
-  
-  u <- exp(sigma*sqrt(dt))
-  d <- exp(-sigma*sqrt(dt))
-  n <- Time/dt
-  p <- (exp(r*dt)-d)/(u-d)
-  
-  Suma <- 0
-  
-  for(j in 0:n){
-    Suma <- Suma + choose(n,j)*p^j*(1-p)^(n-j)*max(S0*u^j*d^(n-j)-K,0)      
-  }
-  
-  return(exp(-r*Time)*Suma)
-}
-
-EOP_call(20,0.12,21,24,1,0.3)
-
-#Ktoś może nam sprzedać
-EOP_put <- function(S0,r,K,Time,dt,sigma){
-  
-  u <- exp(sigma*sqrt(dt))
-  d <- exp(-sigma*sqrt(dt))
-  n <- Time/dt
-  p <- (exp(r*dt)-d)/(u-d)
-  
-  Suma <- 0
-  
-  for(j in 0:n){
-    Suma <- Suma + choose(n,j)*p^j*(1-p)^(n-j)*max(K-S0*u^j*d^(n-j),0)      
-  }
-  
-  return(exp(-r*Time)*Suma)
-}
-
-EOP_put(20,0.12,21,24,1,0.3)
-
-
 Graf <- function(u,d,n=25){
   A<-matrix(0,n,n)
   for(i in 1:n){
@@ -93,19 +54,93 @@ EOP_tree <- function(S0=50,r=0.05,K=52,Time=2,dt=1,sigma=0.3, call=1, EU=1){
   
   
   return(Cofanie[1,1])
-  
-  
-  #Cu <-
-  #Cd <- 
-  #vertex <- Vertex_Value(p,r,dt,Cu,Cd)
 }
+
 EOP_tree(call=0, EU=0)
 
-###test dla dt
+###################### Testy wrażliwości:
 
+#test dla K
+
+vector_K <- seq(40, 60, 1)
+
+values_K_eu_call <- sapply(vector_K, EOP_tree, S0=50,r=0.05,dt=1/12,Time=2,sigma=0.3, call=1, EU=1)
+values_K_eu_put <- sapply(vector_K, EOP_tree, S0=50,r=0.05,dt=1/12,Time=2,sigma=0.3, call=0, EU=1)
+values_K_usa_call <- sapply(vector_K, EOP_tree, S0=50,r=0.05,dt=1/12,Time=2,sigma=0.3, call=1, EU=0)
+values_K_usa_put <- sapply(vector_K, EOP_tree, S0=50,r=0.05,dt=1/12,Time=2,sigma=0.3, call=0, EU=0)
+
+plot(vector_K, values_K_eu_call, type='l', xlab='K', ylab='Cena', main='Cena opcji europejskiej call')
+plot(vector_K, values_K_eu_put, type='l', xlab='K', ylab='Cena', main='Cena opcji europejskiej put')
+plot(vector_K, values_K_usa_call, type='l', xlab='K', ylab='Cena', main='Cena opcji amerykańskiej call')
+plot(vector_K, values_K_usa_put, type='l', xlab='K', ylab='Cena', main='Cena opcji amerykańskiej put')
+
+#test dla T
+
+vector_T <- seq(0.5, 5, 0.5)
+
+values_T_eu_call <- sapply(vector_T, EOP_tree, S0=50,r=0.05,dt=1/12,K=48,sigma=0.3, call=1, EU=1)
+values_T_eu_put <- sapply(vector_T, EOP_tree, S0=50,r=0.05,dt=1/12,K=48,sigma=0.3, call=0, EU=1)
+values_T_usa_call <- sapply(vector_T, EOP_tree, S0=50,r=0.05,dt=1/12,K=48,sigma=0.3, call=1, EU=0)
+values_T_usa_put <- sapply(vector_T, EOP_tree, S0=50,r=0.05,dt=1/12,K=48,sigma=0.3, call=0, EU=0)
+
+plot(vector_T, values_T_eu_call, type='l', xlab='T', ylab='Cena', main='Cena opcji europejskiej call')
+plot(vector_T, values_T_eu_put, type='l', xlab='T', ylab='Cena', main='Cena opcji europejskiej put')
+plot(vector_T, values_T_usa_call, type='l', xlab='T', ylab='Cena', main='Cena opcji amerykańskiej call')
+plot(vector_T, values_T_usa_put, type='l', xlab='T', ylab='Cena', main='Cena opcji amerykańskiej put')
+
+#test dla S0
+
+vector_S0 <- seq(40, 60, 1)
+
+values_S0_eu_call <- sapply(vector_S0, EOP_tree, Time=2,r=0.05,dt=1/12,K=48,sigma=0.3, call=1, EU=1)
+values_S0_eu_put <- sapply(vector_S0, EOP_tree, Time=2,r=0.05,dt=1/12,K=48,sigma=0.3, call=0, EU=1)
+values_S0_usa_call <- sapply(vector_S0, EOP_tree, Time=2,r=0.05,dt=1/12,K=48,sigma=0.3, call=1, EU=0)
+values_S0_usa_put <- sapply(vector_S0, EOP_tree, Time=2,r=0.05,dt=1/12,K=48,sigma=0.3, call=0, EU=0)
+
+plot(vector_S0, values_S0_eu_call, type='l', xlab=expression('S'['0']), ylab='Cena', main='Cena opcji europejskiej call')
+plot(vector_S0, values_S0_eu_put, type='l', xlab=expression('S'['0']), ylab='Cena', main='Cena opcji europejskiej put')
+plot(vector_S0, values_S0_usa_call, type='l', xlab=expression('S'['0']), ylab='Cena', main='Cena opcji amerykańskiej call')
+plot(vector_S0, values_S0_usa_put, type='l', xlab=expression('S'['0']), ylab='Cena', main='Cena opcji amerykańskiej put')
+
+#test dla sigma
+
+vector_sigma <- seq(0.1, 2, 0.1)
+
+values_sigma_eu_call <- sapply(vector_sigma, EOP_tree, S0=50,r=0.05,dt=1/12,K=48,Time=2, call=1, EU=1)
+values_sigma_eu_put <- sapply(vector_sigma, EOP_tree, S0=50,r=0.05,dt=1/12,K=48,Time=2, call=0, EU=1)
+values_sigma_usa_call <- sapply(vector_sigma, EOP_tree, S0=50,r=0.05,dt=1/12,K=48,Time=2, call=1, EU=0)
+values_sigma_usa_put <- sapply(vector_sigma, EOP_tree, S0=50,r=0.05,dt=1/12,K=48,Time=2, call=0, EU=0)
+
+plot(vector_sigma, values_sigma_eu_call, type='l', xlab=expression(sigma), ylab='Cena', main='Cena opcji europejskiej call')
+plot(vector_sigma, values_sigma_eu_put, type='l', xlab=expression(sigma), ylab='Cena', main='Cena opcji europejskiej put')
+plot(vector_sigma, values_sigma_usa_call, type='l', xlab=expression(sigma), ylab='Cena', main='Cena opcji amerykańskiej call')
+plot(vector_sigma, values_sigma_usa_put, type='l', xlab=expression(sigma), ylab='Cena', main='Cena opcji amerykańskiej put')
+
+#test dla r
+
+vector_r <- seq(0.01, 0.2, 0.01)
+
+values_r_eu_call <- sapply(vector_r, EOP_tree, S0=50,Time=2,dt=1/12,K=48,sigma=0.3, call=1, EU=1)
+values_r_eu_put <- sapply(vector_r, EOP_tree, S0=50,Time=2,dt=1/12,K=48,sigma=0.3, call=0, EU=1)
+values_r_usa_call <- sapply(vector_r, EOP_tree, S0=50,Time=2.05,dt=1/12,K=48,sigma=0.3, call=1, EU=0)
+values_r_usa_put <- sapply(vector_r, EOP_tree, S0=50,Time=2,dt=1/12,K=48,sigma=0.3, call=0, EU=0)
+
+plot(vector_r, values_r_eu_call, type='l', xlab='r', ylab='Cena', main='Cena opcji europejskiej call')
+plot(vector_r, values_r_eu_put, type='l', xlab='r', ylab='Cena', main='Cena opcji europejskiej put')
+plot(vector_r, values_r_usa_call, type='l', xlab='r', ylab='Cena', main='Cena opcji amerykańskiej call')
+plot(vector_r, values_r_usa_put, type='l', xlab='r', ylab='Cena', main='Cena opcji amerykańskiej put')
+
+#test dla dt
 
 vector_dt <- seq(0.05, 1, 0.05)
-sapply(vector_dt, EOP_tree, S0=50,r=0.05,K=52,Time=2,sigma=0.3, call=0, EU=0)
 
+values_dt_eu_call <- sapply(vector_dt, EOP_tree, S0=50,r=0.05,K=52,Time=2,sigma=0.3, call=1, EU=1)
+values_dt_eu_put <- sapply(vector_dt, EOP_tree, S0=50,r=0.05,K=52,Time=2,sigma=0.3, call=0, EU=1)
+values_dt_usa_call <- sapply(vector_dt, EOP_tree, S0=50,r=0.05,K=52,Time=2,sigma=0.3, call=1, EU=0)
+values_dt_usa_put <- sapply(vector_dt, EOP_tree, S0=50,r=0.05,K=52,Time=2,sigma=0.3, call=0, EU=0)
 
+plot(vector_dt, values_dt_eu_call, type='l', xlab='dt', ylab='Cena', main='Cena opcji europejskiej call')
+plot(vector_dt, values_dt_eu_put, type='l', xlab='dt', ylab='Cena', main='Cena opcji europejskiej put')
+plot(vector_dt, values_dt_usa_call, type='l', xlab='dt', ylab='Cena', main='Cena opcji amerykańskiej call')
+plot(vector_dt, values_dt_usa_put, type='l', xlab='dt', ylab='Cena', main='Cena opcji amerykańskiej put')
 
